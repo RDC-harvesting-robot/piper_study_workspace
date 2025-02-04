27a8b834d35e2344c176f6c73d10859e163fe9eb
yasuohayashibara <yasuo.hayashibara@it-chiba.ac.jp>
2025-02-04 19:43:48 +0900

    modify

--- piper_study/scripts/pick_and_place.py
+++ piper_study/scripts/pick_and_place.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 import rospy
 from moveit_commander import MoveGroupCommander, PlanningSceneInterface, RobotCommander
-from geometry_msgs.msg import Pose
+from geometry_msgs.msg import Pose, Point, Quaternion
+from tf.transformations import quaternion_from_euler, quaternion_multiply
+import copy
 
 if __name__ == "__main__":
     rospy.init_node("move_group_interface_tutorial", anonymous=True)
@@ -16,77 +18,71 @@ if __name__ == "__main__":
     rospy.loginfo(robot.get_group_names())
 
     # 1. Move to home position
-    # move_group_arm.set_named_target("home")
-    # plan_arm = move_group_arm.plan()
-    # success = move_group_arm.execute(plan_arm, wait=True)
-    # rospy.loginfo("Moving to home position: %s", "SUCCESS" if success else "FAILED")
-
-    # 2. Place the TCP above the blue box
-
-    target_joint = [0.0, 0.01, -0.01, 0.0, 0.0, 0.0]
-    move_group_arm.set_joint_value_target(target_joint)
+    move_group_arm.set_named_target("home")
     move_group_arm.go(wait=True)
     move_group_arm.stop()
-
-    # 2. Place the TCP above the blue box
-    target_joint = [1.57, 0.01, -0.01, 0.0, 0.0, 0.0]
-    move_group_arm.set_joint_value_target(target_joint)
-    move_group_arm.go(wait=True)
-    move_group_arm.stop()
-
-    print(move_group_arm.get_current_pose().pose)
+    move_group_arm.clear_pose_targets()
 
     # 2. Place the TCP above the blue box
     current_pose = move_group_arm.get_current_pose().pose
-    target_pose1 = Pose()
-    target_pose1.orientation = current_pose.orientation
-    target_pose1.position.x = current_pose.position.x
-    target_pose1.position.y = current_pose.position.y
-    target_pose1.position.z = current_pose.position.z + 0.2
+    target_pose1 = copy.deepcopy(current_pose)
+    target_pose1.position.x += 0.29
+    target_pose1.position.y -= 0.2
+    target_pose1.position.z -= 0.15
+    target_pose1.orientation = target_pose1.orientation.__class__(
+        *quaternion_multiply(
+            [current_pose.orientation.x, current_pose.orientation.y, current_pose.orientation.z, current_pose.orientation.w],
+            quaternion_from_euler(0, 1.57, 0)
+        )
+    )
     move_group_arm.set_pose_target(target_pose1)
     move_group_arm.go(wait=True)
     move_group_arm.stop()
     move_group_arm.clear_pose_targets()
 
-    # 3. Open the gripper
+    # 4. Close the gripper
     move_group_gripper.set_named_target("open")
-    plan_gripper = move_group_gripper.plan()
-    success = move_group_gripper.execute(plan_gripper, wait=True)
-    rospy.loginfo("Opening gripper: %s", "SUCCESS" if success else "FAILED")
+    move_group_gripper.go(wait=True)
+    move_group_gripper.stop()
+    move_group_gripper.clear_pose_targets()
 
-    # 4. Move the TCP close to the object
-    target_pose1.position.z -= 0.2
+    # 3. Move the TCP close to the object
+    target_pose1.position.z -= 0.05
     move_group_arm.set_pose_target(target_pose1)
-    plan_arm = move_group_arm.plan()
-    success = move_group_arm.execute(plan_arm, wait=True)
-    rospy.loginfo("Moving TCP close to the object: %s", "SUCCESS" if success else "FAILED")
-
-    # 5. Close the gripper
+    move_group_arm.go(wait=True)
+    move_group_arm.stop()
+    move_group_arm.clear_pose_targets()
+    
+    # 4. Close the gripper
     move_group_gripper.set_named_target("closed")
-    plan_gripper = move_group_gripper.plan()
-    success = move_group_gripper.execute(plan_gripper, wait=True)
-    rospy.loginfo("Closing gripper: %s", "SUCCESS" if success else "FAILED")
+    move_group_gripper.go(wait=True)
+    move_group_gripper.stop()
+    move_group_gripper.clear_pose_targets()
+
+    # 5. Move the TCP above the plate
+    target_pose1.position.z += 0.2
+    move_group_arm.set_pose_target(target_pose1)
+    move_group_arm.go(wait=True)
+    move_group_arm.stop()
+    move_group_arm.clear_pose_targets()
 
     # 6. Move the TCP above the plate
-    target_pose1.position.z += 0.2
-    target_pose1.position.x -= 0.6
+    target_pose1.position.y += 0.4
     move_group_arm.set_pose_target(target_pose1)
-    plan_arm = move_group_arm.plan()
-    success = move_group_arm.execute(plan_arm, wait=True)
-    rospy.loginfo("Moving TCP above the plate: %s", "SUCCESS" if success else "FAILED")
+    move_group_arm.go(wait=True)
+    move_group_arm.stop()
+    move_group_arm.clear_pose_targets()
 
     # 7. Lower the TCP above the plate
     target_pose1.position.z -= 0.14
     move_group_arm.set_pose_target(target_pose1)
-    plan_arm = move_group_arm.plan()
-    success = move_group_arm.execute(plan_arm, wait=True)
-    rospy.loginfo("Lowering TCP above the plate: %s", "SUCCESS" if success else "FAILED")
+    move_group_arm.go(wait=True)
+    move_group_arm.stop()
+    move_group_arm.clear_pose_targets()
 
     # 8. Open the gripper
     move_group_gripper.set_named_target("open")
-    plan_gripper = move_group_gripper.plan()
-    success = move_group_gripper.execute(plan_gripper, wait=True)
-    rospy.loginfo("Opening gripper: %s", "SUCCESS" if success else "FAILED")
+    move_group_gripper.go(wait=True)
+    move_group_gripper.stop()
+    move_group_gripper.clear_pose_targets()
 
-    rospy.loginfo("Task completed!")
-    rospy.signal_shutdown("Task completed")
